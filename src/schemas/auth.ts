cdf66c1a7446472b71cf7c7d452a472ae7990f5e
Sprite <sprite.ktd@gmail.com>
2024-01-21 21:06:49 +0700

    update

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -3,5 +3,10 @@ import { z } from 'zod';
 export const authSchema = z.object({
   email: z.string().email(),
 });
-
 export type AuthSchema = z.infer<typeof authSchema>;
+
+export const confirmationSchema = z.object({
+  code: z.string().length(6),
+  confirmEmail: z.string().email(),
+});
+export type ConfirmationSchema = z.infer<typeof confirmationSchema>;
